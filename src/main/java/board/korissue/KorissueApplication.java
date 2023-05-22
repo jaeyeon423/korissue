a68d412147cce8c3351d321568064a041d2ca7e8
jaeyeon <jaeyeon423@gmail.com>
2023-05-23 08:10:28 +0900

    project create

---
+++ src/main/java/board/korissue/KorissueApplication.java
@@ -0,0 +1,13 @@
+package board.korissue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KorissueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KorissueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/board/korissue/KorissueApplicationTests.java
@@ -0,0 +1,13 @@
+package board.korissue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KorissueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
