90fa758613e6fc85e9ddfef7afbeb0c50cf83fa3
jaeyeon <jaeyeon423@gmail.com>
2023-05-23 12:24:00 +0900

    build.gradle update

---
+++ src/main/java/board/korissue/controller/DemoController.java
@@ -0,0 +1,14 @@
+package board.korissue.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/demo")
+    public String demo(){
+        return "demo";
+    }
+
+}
